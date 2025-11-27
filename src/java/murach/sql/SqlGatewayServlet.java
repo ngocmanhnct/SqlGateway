04a46e4775b5110f91dc46d349ef4ca5b5487fe0
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2025-11-27 15:30:00 +0700

    initital commit

---
+++ src/java/murach/sql/SqlGatewayServlet.java
@@ -0,0 +1,72 @@
+package murach.sql;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.sql.*;
+
+public class SqlGatewayServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String sqlStatement = request.getParameter("sqlStatement");
+        String sqlResult = "";
+        try {
+            // load the driver
+            Class.forName("org.postgresql.Driver");
+// get a connection
+            String dbURL = "jdbc:postgresql://dpg-d4k09qm3jp1c738gen50-a.oregon-postgres.render.com:5432/ltweb";
+            String username = "ltweb_user";
+            String password = "VZcuMQTrnePaaCtIsNMr2rzmhiksPp5h";
+            Connection connection = DriverManager.getConnection(
+                    dbURL, username, password);
+
+            // create a statement
+            Statement statement = connection.createStatement();
+
+            // parse the SQL string
+            sqlStatement = sqlStatement.trim();
+            if (sqlStatement.length() >= 6) {
+                String sqlType = sqlStatement.substring(0, 6);
+
+                if (sqlType.equalsIgnoreCase("select")) {
+                    // create the HTML for the result set
+                    ResultSet resultSet
+                            = statement.executeQuery(sqlStatement);
+                    sqlResult = SQLUtil.getHtmlTable(resultSet);
+                    resultSet.close();
+                } else {
+                    int i = statement.executeUpdate(sqlStatement);
+                    if (i == 0) { // a DDL statement
+                        sqlResult = 
+                          "<p>The statement executed successfully.</p>";
+                    } else { // an INSERT, UPDATE, or DELETE statement
+                        sqlResult = 
+                          "<p>The statement executed successfully.<br>"
+                                + i + " row(s) affected.</p>";
+                    }
+                }
+            }
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            sqlResult = "<p>Error loading the database driver: <br>"
+                    + e.getMessage() + "</p>";
+        } catch (SQLException e) {
+            sqlResult = "<p>Error executing the SQL statement: <br>"
+                    + e.getMessage() + "</p>";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("sqlResult", sqlResult);
+        session.setAttribute("sqlStatement", sqlStatement);
+
+        String url = "/index.jsp";
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+}
